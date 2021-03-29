cfe65d22d302d95526a5862d770dc06de82b7a8a
Sen Hung Wong <alexwongsenhung@gmail.com>
2021-03-29 15:39:58 +0800

    Initialize

---
+++ server.js
@@ -0,0 +1,9 @@
+const { PeerServer } = require("peer");
+
+const generateClientId = () =>
+  Math.floor(Math.random() * 10000)
+    .toString()
+    .padStart(4, "0");
+const peerServer = PeerServer({ port: 9000, path: "/", generateClientId });
+peerServer.on("connection", (c) => console.log(`${c.getId()} connected`));
+peerServer.on("disconnect", (c) => console.log(`${c.getId()} disconnected`));
